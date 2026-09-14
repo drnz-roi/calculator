e765adb0319099b0d960ee438a4ec553ff323c19
“dnz-roi” <drisa100k@gmail.com>
2026-09-13 20:42:36 -0400

    Add core calculator math functions and operate() dispatcher

---
+++ calculator.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Nice try, but dividing by zero breaks the universe.";
+  }
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return null;
+  }
+}
+
+module.exports = { add, subtract, multiply, divide, operate };
